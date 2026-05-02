863f24eaa75b684799ac910f179928cd2730b700
Cavaldos <khanhnguyen297kn@gmail.com>
2026-05-02 13:34:05 +0700

    config docker

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,8 +4,6 @@ import cors from "cors";
 import morgan from "morgan";
 import routers from "./api/routes";
 import { Request, Response } from "express";
-import swaggerUi from "swagger-ui-express";
-import { swaggerSpec, swaggerUiOptions } from "./config/swagger";
 
 const ALLOWED_ORIGINS = process.env.CORS_ORIGINS
   ? process.env.CORS_ORIGINS.split(',').map(origin => origin.trim()).filter(Boolean)
@@ -27,19 +25,12 @@ app.use(json());
 app.use(helmet());
 app.use(morgan('tiny'));
 
-// Swagger UI
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerUiOptions));
-app.get("/api-docs.json", (_req: Request, res: Response) => {
-  res.setHeader("Content-Type", "application/json");
-  res.send(swaggerSpec);
-});
 
 app.use(routers);
 
 
 app.get("/", (_req: Request, res: Response) => {
-  //create button to Guide directly to Swagger docs
-  res.send("<h1>Hello , Welcome to my server</h1><p><a href='/api-docs'>View API Docs</a></p>");
+  res.send("<h1>Hello , Welcome to my server</h1>");
 
 });
 export default app;
